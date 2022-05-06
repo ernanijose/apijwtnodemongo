4e92cd2ddaf6ecd46b0f2dffb3c6618a28952347
ernanijose <ernanijose@gmail.com>
2022-05-05 22:47:33 -0300

    Verificando e-mail

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,6 @@ const router = require('express').Router();
 const User = require('../model/User');
 const { registerValidation, loginValidation } = require("../validation");
 
-// //Validação com JOI
-// const Joi = require('@hapi/joi');
-
-// const userValidation = Joi.object({
-//     name: Joi.string().min(6).required(),
-//     email: Joi.string().min(6).required().email(),
-//     password: Joi.string().min(6).required()
-// });
-
 router.post("/register", async (req, res) => {
     //aqui efetuamos a validação dos dados enviados
     //const validation = Joi.validate(req.body, userValidation);
@@ -21,6 +12,14 @@ router.post("/register", async (req, res) => {
         return res.status(400).send(error);
     }
 
+    //verificando se o e-mail existe no banco de dados
+    const emailExists = await User.findOne({
+        email: req.body.email
+    });
+    if(emailExists) return res.status(400).send("Este e-mail está cadastrado, tente outro e-mail");
+
+
+    //Criando um novo usuário
     const user = new User({
         name: req.body.name,
         email: req.body.email,
